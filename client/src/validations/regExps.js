f5317ea85a0114bbc89142dbd207b1793653ae93
Okke Harsta <oharsta@zilverline.com>
2025-09-23 13:16:23 +0200

    Fixes #77

--- client/src/validations/regExps.js
+++ client/src/validations/regExps.js
@@ -5,7 +5,7 @@ export const validEmailRegExp = /^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.?[a-zA-Z]*$/;
 export const validUrlRegExp = /(https?|ssh|ftp):\/\/(((www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.?[a-z]{0,63})|(localhost))\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/i
 
 export const isValidUrl = url => {
-    //We allow for empty URL's, enforcing required is different responsibility
+    //We allow for empty URL's, enforcing required is a different responsibility
     return isEmpty(url) || (validUrlRegExp.test(url) && (url.startsWith("https")
         || (url.startsWith("http://localhost") || url.startsWith("http://127.0.0.1"))));
 }
