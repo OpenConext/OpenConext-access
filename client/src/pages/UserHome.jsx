36c99ddf4b196e2044ba7364e75e5c196cf01dd3
Okke Harsta <oharsta@zilverline.com>
2026-03-06 09:12:21 +0100

    Upped dependencies

--- client/src/pages/UserHome.jsx
+++ client/src/pages/UserHome.jsx
@@ -31,7 +31,7 @@ const UserHome = () => {
                 ]
             });
         }
-    }, []);
+    }, [newLocation]);
 
     if (newLocation !== null) {
         return <Navigate to={newLocation} replace/>;
