cb4ebd8e98f87fed16ecae47cfdb5c44bf2eecc4
Okke Harsta <oharsta@gmail.com>
2026-08-15 10:14:53 +0200

    Bugfix for accepting invitation

--- client/src/utils/Organization.js
+++ client/src/utils/Organization.js
@@ -1,7 +1,7 @@
 export function currentOrganizationFromUser(user, organizationId) {
     organizationId = parseInt(organizationId, 10);
     const membership = user.organizationMemberships.find(orgMembership => orgMembership.organization.id === organizationId);
-    //Deliberate fail when the organization is not found, will be handled in try / catch, navigat("404")
-    return membership.organization;
+    //Deliberate fail when the organization is not found, will be handled upstream
+    return membership?.organization;
 
 }
