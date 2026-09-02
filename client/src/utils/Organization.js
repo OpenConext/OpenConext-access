7f019732e2b48df85db4b9d751ee016d462849c9
Okke Harsta <oharsta@gmail.com>
2026-09-02 11:53:55 +0200

    Fix for spinner in start

--- client/src/utils/Organization.js
+++ client/src/utils/Organization.js
@@ -4,6 +4,9 @@ export function currentOrganizationFromUser(user, organizationId) {
     organizationId = parseInt(organizationId, 10);
     const membership = user.organizationMemberships.find(orgMembership => orgMembership.organization.id === organizationId);
     //Do not fail if the organization is not found
+    if (isEmpty(user.organizationMemberships)) {
+        return null;
+    }
     if (isEmpty(membership)) {
         const organization = user.organizationMemberships[0].organization;
         localStorage.setItem("organization", organization.id.toString());
